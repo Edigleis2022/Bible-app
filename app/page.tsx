59198e758d811b35b0df32fb560470b0b7a199ec
Edigleis <105567136+Edigleis2022@users.noreply.github.com>
2026-02-09 15:54:23 -0400

    🚧feat: melhoria no layout e organização dos botões + ajustes visuais no Bible App

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,74 @@
-'use client'
+"use client";
 
-import { useState, useEffect } from 'react'
-import VerseCard from '@/components/VerseCard'
-import { verses } from '@/data/verses'
-import { Verse } from '@/types/verse'
-import './page.scss'
-import Link from 'next/link'
+import { useState, useEffect } from "react";
+import VerseCard from "@/components/VerseCard";
+import { verses } from "@/data/verses";
+import { Verse } from "@/types/verse";
+import "./page.scss";
+import Link from "next/link";
 
+function getVerseOfDay(verses: Verse[]) {
+    const today = new Date().toDateString(); // data de hoje
+    const saved = localStorage.getItem("verseDay");
+
+    if (saved) {
+      const parsed = JSON.parse(saved);
+
+      // se já salvou hoje, retorna o mesmo
+      if (parsed.date === today) {
+        return parsed.verse;
+      }
+    }
+
+    // sorteia versículo baseado no dia
+    const index = new Date().getDate() % verses.length;
+    const verse = verses[index];
+
+    localStorage.setItem(
+      "verseDay",
+      JSON.stringify({
+        date: today,
+        verse: verse,
+      }),
+    );
+
+    return verse;
+  }
 
 export default function Home() {
-
-  const [loading, setLoading] = useState(false)
-
+  const [loading, setLoading] = useState(false);
+  const [currentVerse, setCurrentVerse] = useState<Verse | null>(null);
+  const [verseDay, setVerseDay] = useState<Verse | null>(null);
 
   // 🎯 Versículo aleatório inicial
-  const [index, setIndex] = useState(
-    Math.floor(Math.random() * verses.length)
-  )
+  const [index, setIndex] = useState(Math.floor(Math.random() * verses.length));
 
   // ❤️ Lista de favoritos
-  const [favorites, setFavorites] = useState<Verse[]>([])
-  
-
+  const [favorites, setFavorites] = useState<Verse[]>([]);
 
   // 🌙 Tema escuro
   // começa false
-const [dark, setDark] = useState(false)
+  const [dark, setDark] = useState(false);
 
-// carrega tema só no navegador
-useEffect(() => {
-  const savedTheme = localStorage.getItem("theme");
-  if (savedTheme === "dark") {
-    setDark(true);
-  }
-}, []);
+  // carrega tema só no navegador
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "dark") {
+      setDark(true);
+    }
+  }, []);
 
   // 📦 Carregar favoritos do localStorage quando abrir app
   useEffect(() => {
-    const saved = localStorage.getItem('favorites')
+    const saved = localStorage.getItem("favorites");
 
     if (saved) {
-      setFavorites(JSON.parse(saved))
+      setFavorites(JSON.parse(saved));
     }
 
-    const theme = localStorage.getItem('theme')
-    if (theme === 'light') setDark(false)
-  }, [])
+    const theme = localStorage.getItem("theme");
+    if (theme === "light") setDark(false);
+  }, []);
 
   // 💾 Sempre que favoritos mudar, salva no navegador
   useEffect(() => {
@@ -57,55 +80,90 @@ useEffect(() => {
     localStorage.setItem("theme", dark ? "dark" : "light");
   }, [dark]);
 
+  useEffect(() => {
+    const verse = getVerseOfDay(verses);
+    setVerseDay(verse);
+  }, []);
 
   // 🔁 Novo versículo
   function newVerse() {
-  setLoading(true)
+    setLoading(true);
 
-  setTimeout(() => {
-    const random = Math.floor(Math.random() * verses.length)
-    setIndex(random)
-    setLoading(false)
-  }, 400) // tempo da animação
-}
-
+    setTimeout(() => {
+      const random = Math.floor(Math.random() * verses.length);
+      setIndex(random);
+      setLoading(false);
+    }, 400); // tempo da animação
+  }
 
   // ❤️ Favoritar versículo
   function toggleFavorite(verse: Verse) {
-    const exists = favorites.find(v => v.id === verse.id)
+    const exists = favorites.find((v) => v.id === verse.id);
 
     if (exists) {
       // remove se já existe
-      setFavorites(favorites.filter(v => v.id !== verse.id))
+      setFavorites(favorites.filter((v) => v.id !== verse.id));
     } else {
       // adiciona
-      setFavorites([...favorites, verse])
+      setFavorites([...favorites, verse]);
     }
   }
 
+  async function fetchVerse() {
+    try {
+      setLoading(true);
+
+      const res = await fetch("https://bible-api.com/john+3:16");
+      const data = await res.json();
+
+      const verseApi = {
+        id: Date.now(),
+        text: data.text,
+        reference: data.reference,
+      };
+
+      // coloca como versículo atual
+      setCurrentVerse(verseApi);
+
+      setLoading(false);
+    } catch (error) {
+      console.log("Erro ao buscar versículo", error);
+      setLoading(false);
+    }
+  }
+  
   return (
     <main className={dark ? "container dark" : "container"}>
-      <h1 className="title">📖 Bible App</h1>
+      <h1 className="title">📖 Aplicativo da Bíblia</h1>
 
-      <Link href="/favorites">
-        <button className="btn-fav-page">Ver favoritos ⭐</button>
-      </Link>
+      <div className="buttons">
+        <Link href="/favorites">
+          <button className="btn-fav-page">⭐ Ver favoritos</button>
+        </Link>
 
-      {/* botão tema */}
-      <button className="theme-btn" onClick={() => setDark(!dark)}>
-        {dark ? "🌙 Dark" : "☀️ Light"}
-      </button>
+        <button className="theme-btn" onClick={() => setDark(!dark)}>
+          {dark ? "🌙 Dark" : "☀️ Luz"}
+        </button>
 
-      <VerseCard
-        verse={verses[index]}
-        onFavorite={toggleFavorite}
-        isFavorite={favorites.some((v) => v.id === verses[index].id)}
-        loading={loading}
-      />
+        <button className="btn-api" onClick={fetchVerse}>
+          🌐 Buscar versículo online
+        </button>
 
-      <button className="btn-new" onClick={newVerse}>
-        Novo versículo
-      </button>
+        <button className="btn-new" onClick={newVerse}>
+          ✨ Novo versículo
+        </button>
+      </div>
+
+      {(currentVerse || verseDay) && (
+        <VerseCard
+          verse={currentVerse || verseDay!}
+          onFavorite={toggleFavorite}
+          isFavorite={favorites.some(
+            (v) => v.id === (currentVerse || verseDay!)?.id,
+          )}
+          loading={loading}
+        />
+      )}
     </main>
   );
 }
--- data/verses.ts
+++ data/verses.ts
@@ -8,12 +8,82 @@ export const verses: Verse[] = [
     reference: "Filipenses 4:13"
   },
   {
-    id: 2,
+  id: 2,
+  text: "O Senhor é o meu pastor; nada me faltará.",
+  reference: "Salmos 23:1"
+},
+{
+  id: 3,
+  text: "Entrega o teu caminho ao Senhor; confia nele, e ele o fará.",
+  reference: "Salmos 37:5"
+},
+{
+  id: 4,
+  text: "Se Deus é por nós, quem será contra nós?",
+  reference: "Romanos 8:31"
+},
+{
+  id: 5,
+  text: "Porque sou eu que conheço os planos que tenho para vocês.",
+  reference: "Jeremias 29:11"
+},
+{
+  id: 6,
+  text: "Buscai primeiro o Reino de Deus e a sua justiça.",
+  reference: "Mateus 6:33"
+},
+{
+  id: 7,
+  text: "Sede fortes e corajosos; não temais.",
+  reference: "Josué 1:9"
+},
+{
+  id: 8,
+  text: "O choro pode durar uma noite, mas a alegria vem pela manhã.",
+  reference: "Salmos 30:5"
+},
+{
+  id: 9,
+  text: "A fé remove montanhas.",
+  reference: "Mateus 17:20"
+},
+{
+  id: 10,
+  text: "Deem graças ao Senhor, porque ele é bom.",
+  reference: "Salmos 107:1"
+},
+{
+  id: 11,
+  text: "Tudo tem o seu tempo determinado.",
+  reference: "Eclesiastes 3:1"
+},
+{
+  id: 12,
+  text: "O Senhor lutará por vocês.",
+  reference: "Êxodo 14:14"
+},
+{
+  id: 13,
+  text: "Bem-aventurados os que creem.",
+  reference: "João 20:29"
+},
+{
+  id: 14,
+  text: "Deus é o nosso refúgio e fortaleza.",
+  reference: "Salmos 46:1"
+},
+{
+  id: 15,
+  text: "Aquietai-vos e sabei que eu sou Deus.",
+  reference: "Salmos 46:10"
+},
+  {
+    id:16,
     text: "O Senhor é meu pastor e nada me faltará.",
     reference: "Salmos 23:1"
   },
   {
-    id: 3,
+    id:16,
     text: "Se Deus é por nós, quem será contra nós?",
     reference: "Romanos 8:31"
   }
